9efac2db6ca4ce393d99befb37c25021fdfc0867
kangshinsok <https://github.com/kangshinsok>
2022-06-22 11:35:21 +0900

    수정3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 수정중입니다
+
+
+//사용자 설정
+//깃을 사용할때 사용하는 깃의 용자 셋팅
+//git config --global user.name "자기 닉 네임 "
+//git config --global user.email  " 자기 이메일" 
+
+//설정 정보 조회
+//gif 설정아 제대로 되었는 확인
+//gif config --global --list
+
+//gif 저장소 초기화 저장소 생성
+//gif의 저장소로 지정헐 경로로  이동해서
+//gif init 
+
+//gif 저장소 복제 (파일 내려 받기)
+//gif clone (url)
+
+
+
+
+
+//gif 저장소
+
+
+
+
